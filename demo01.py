7de9f9a8f6f1d0d23428322f3ecf0a66828cea67
siterow <823978463@qq.com>
2020-08-11 14:19:16 +0800

    第三次提交

--- demo01.py
+++ demo01.py
@@ -4,10 +4,14 @@ print("hello world")
 print(233333)
 print(True)
 print(())
-
+8.10 print
+'''
 '''
 a=int(input())
 b=int(input())
 print(a+b,a-b,a*b,a/b)
 print(type(a))
 print(type(b))
+8.10 input
+'''
+
